a601dd897ca8cfb59c366efab1d0ee264948efd0
zloco <aploco1@etf.unsa.ba>
2017-01-31 11:08:56 +0100

    changed / into * in app.js just for testing

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
- app.get('/', function(req, res, next) {
+ app.get('*', function(req, res, next) {
      res.redirect('/html/login.html');
  });
 
